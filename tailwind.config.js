50f06ba572814d7b5054a73c1c8b3f7a2b8bb428
Gagandeep <gag7550@gmail.com>
2020-07-21 21:52:51 +0530

    updated tailwind config full

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+      fontFamily: {
+        // body: ["Nunito"],
+      },
+    },
   },
   variants: {},
   plugins: [],
